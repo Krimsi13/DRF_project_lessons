61cebbdb8911cf2d71c6de3d25400727118cc7fe
Krimsi13 <aleksejkrimsi@gmail.com>
2024-06-07 22:32:00 +0300

    .\.env

--- materials/models.py
+++ materials/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
-NULLABLE = {'null': True, 'blank': True}
+NULLABLE = {"null": True, "blank": True}
 
 
 class Course(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название курса")
-    preview = models.ImageField(upload_to="materials/preview/", **NULLABLE, verbose_name="Картинка")
+    preview = models.ImageField(
+        upload_to="materials/preview/", **NULLABLE, verbose_name="Картинка"
+    )
     description = models.TextField(verbose_name="Описание курса")
     # lessons = models.ManyToManyField(Lesson, verbose_name="Уроки")
 
@@ -17,12 +19,15 @@ class Course(models.Model):
 class Lesson(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название урока")
     description = models.TextField(verbose_name="Описание урока")
-    preview = models.ImageField(upload_to="materials/preview/", **NULLABLE, verbose_name="Картинка")
+    preview = models.ImageField(
+        upload_to="materials/preview/", **NULLABLE, verbose_name="Картинка"
+    )
     link_to_video = models.TextField(verbose_name="Ссылка на видео")
 
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс")
+    course = models.ForeignKey(
+        Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс"
+    )
 
     class Meta:
         verbose_name = "Урок"
         verbose_name_plural = "Уроки"
-
--- users/models.py
+++ users/models.py
@@ -1,17 +1,31 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-NULLABLE = {'null': True, 'blank': True}
+NULLABLE = {"null": True, "blank": True}
 
 
 class User(AbstractUser):
     username = None
 
     email = models.EmailField(unique=True, verbose_name="Email", max_length=255)
-    phone = models.CharField(max_length=35, verbose_name="Телефон", **NULLABLE, help_text="Введите номер телефона")
-    city = models.CharField(max_length=255, verbose_name="Страна", **NULLABLE, help_text="Введите свою страну")
-    avatar = models.ImageField(upload_to='users/avatars', verbose_name="Аватар", **NULLABLE,
-                               help_text="Загрузите свой аватар")
+    phone = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        **NULLABLE,
+        help_text="Введите номер телефона"
+    )
+    city = models.CharField(
+        max_length=255,
+        verbose_name="Страна",
+        **NULLABLE,
+        help_text="Введите свою страну"
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        verbose_name="Аватар",
+        **NULLABLE,
+        help_text="Загрузите свой аватар"
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
